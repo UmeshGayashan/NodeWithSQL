ddd835ddb9b59cf638c64e9ce60d075899cab400
Umesh Gayashan <138327662+UmeshGayashan@users.noreply.github.com>
2024-10-11 13:16:32 +0530

    create get prodcuts route with jsonify category details

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+// Load environment variables from .env file
+require('dotenv').config();
 const express = require('express');
 const database = require('./services/database');
 const pool = require('./services/database');
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => { 
     res.send('Hello World');
@@ -16,6 +19,18 @@ app.get('/categories', async(req, res) => {
     }
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.get('/products', async(req, res) => { 
+    try {
+        const result = await pool.query(`SELECT 
+        p.id,p.name,p.description,p.price,p.currency,p.quantity,p.active,p.created_date,p.updated_date,
+        (SELECT ROW_TO_JSON(category_obj)FROM(SELECT id,name FROM category WHERE id = p.category_id)category_obj) AS category
+        FROM product p`);
+        return res.status(200).json(result.rows);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}.`);
 });
