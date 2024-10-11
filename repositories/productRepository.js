e7ee2480d9bb9125a72704d927b071255b841fd0
Umesh Gayashan <138327662+UmeshGayashan@users.noreply.github.com>
2024-10-12 00:40:35 +0530

    create Product Route

---
+++ repositories/productRepository.js
@@ -0,0 +1,21 @@
+const pool = require('../services/database');
+
+const createProductInDB = async (name, decription, price, currency, quantity, active, category_id) => {
+    const result = await pool.query({
+        text: `INSERT INTO product(name, description, price, currency, quantity, active, category_id) 
+            VALUES($1, $2, $3, $4, $5, $6, $7) 
+            RETURNING *`,
+        values: [name, 
+            decription ? decription : null, 
+            price, 
+            currency ? currency : 'LKR', 
+            quantity ? quantity : 0, 
+            active ? active : true, 
+            category_id]
+    });
+    return result.rows[0];
+};
+
+module.exports = {
+    createProductInDB
+};
