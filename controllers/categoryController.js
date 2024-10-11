fe00a2ea27622b9a46819b5052cc1a13dcdb0163
Umesh Gayashan <138327662+UmeshGayashan@users.noreply.github.com>
2024-10-11 16:01:59 +0530

    If there is no name value give specified error

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -12,6 +12,9 @@ const getCategoriesController = async (req, res) => { // This is async because i
 const createCategoryController = async (req, res) => {
     const { name } = req.body;
     try {
+        if (!name) {
+            return res.status(400).json({ error: 'Name is required' }); // 400 for bad request
+        }
         const newCategory = await createCategory(name);
         return res.status(201).json(newCategory); // 201 for successful creation
     } catch (error) {
