7da3d6a35d30a26ec441a0ff8690a2aa079d262b
Umesh Gayashan <138327662+UmeshGayashan@users.noreply.github.com>
2024-10-12 22:50:00 +0530

    Request params validation should be added to controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,6 +2,17 @@ const { createProduct } = require('../services/productService')
 
 const createProductController = async (req,res) =>{
     const {name, description, price, currency, quantity, active, category_id} = req.body;
+    
+    if(!name){
+        return res.status(422).json({ error: 'Name is required' });
+    }
+    if(!price){
+        return res.status(422).json({ error: 'Price is required' });
+    }
+    if(!category_id){
+        return res.status(422).json({ error: 'Category ID is required' });
+    }
+
     try{
         const newProduct = await createProduct(name, description, price, currency, quantity, active, category_id);
         return res.status(201).json(newProduct);
